5e6604c95393ae3f4e986374aa31f5bfc36f534b
715018 <715018@student.dusd.net>
2022-11-15 08:44:17 -0800

    new script
    created new scene and new script

---
+++ ScriptingProjectAyala/Assets/Scripts/AwakeAndStart.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AwakeAndStart : MonoBehaviour
+{
+    void Awake ()
+        //Set Ammo for the enemy
+    {
+        Debug.Log("Start called");
+    }
+    // Start is called before the first frame update
+    void Start()
+        //Allow enemy to shoot
+    {
+        Debug.Log("Start called");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
