9cac712234f9d80ca0f8c6de352ca277113f98fb
715018 <715018@student.dusd.net>
2022-11-30 09:57:03 -0800

    new script
    update and fixed update script

--- ScriptingProjectAyala/Assets/Scripts/UpdateAndFixedUpdate.cs
+++ ScriptingProjectAyala/Assets/Scripts/UpdateAndFixedUpdate.cs
@@ -2,23 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AwakeAndStart : MonoBehaviour
+public class UpdateAndFixedUpdate : MonoBehaviour
 {
-    void Awake ()
-        //Set Ammo for the enemy
+   void FixedUpdate ()
     {
-        Debug.Log("Awake is Called");
+        Debug.Log("FixedUpdate time :" + Time.deltaTime);
     }
+
+    
     // Start is called before the first frame update
     void Start()
-        //Allow enemy to shoot
     {
-        Debug.Log("Start called");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("Update time :" + Time.deltaTime);
     }
 }
